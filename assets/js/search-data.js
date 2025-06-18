cb0d9596b39ed1677c5f1decb6ad475ea0413e05
zjgcjy <zjgcjy@outlook.com>
2025-06-18 08:08:46 +0000

    Deploying to gh-pages from @ zjgcjy/blog@b068afe4c75b326c5e70a1b3c32b32b00efebb8a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-",
-    title: "",
+    id: "nav-about",
+    title: "about",
     section: "Navigation",
     handler: () => {
       window.location.href = "/blog/";
@@ -14,7 +14,7 @@ ninja.data = [{
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/blog/index.html";
+            window.location.href = "/blog/posts/index.html";
           },
         },{id: "nav-projects",
           title: "projects",
